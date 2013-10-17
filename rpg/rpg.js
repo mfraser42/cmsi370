ae53f2260146723399bb768d6330c28535932254
Michael Fraser <mfraser702@gmail.com>
2013-10-16 21:45:53 -0700

    Character page layout, random item spawner and inventory added

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -6,6 +6,22 @@ $(function () {
         console.log("Delete confirmed!");
         $('#deleteModal').modal('hide');
     });
+    
+    var inv_items = new Array();
+    inv_items.push("Wirt's Leg");
+    inv_items.push("Potato");
+    
+    var random_items = ["A Crooked Seven", "Pocket Lint", "A Microprocessor", "A Left Shoe", "A Pen", "Scroll on Interaction Design"];
+    
+    $( "#random_item" ).click(function () {
+        if (inv_items.length == 9) {
+            alert("Inventory is full.");
+        } else {
+            var rand_index = Math.floor(Math.random()*random_items.length);
+            $( "#inventory" ).append("<tr> <td> " + random_items[rand_index] + " </td> </tr>");
+            inv_items.push(random_items[rand_index]);
+        }
+    });
 
 });
 
