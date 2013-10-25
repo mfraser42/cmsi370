e2a452208ee531c31411ddf980d5949d34e36f99
Michael Fraser <mfraser702@gmail.com>
2013-10-24 23:32:52 -0700

    Add and delete characters fixed

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -4,16 +4,6 @@ $(function () {
     var char_names = ["Slender", "AquaMan", "Honey Badger", "Potted Plant"];
     var char_levels = ["7", "22", "9001", "42"];
     var char_types = ["Creeper", "Underappreciated Hero", "Beast", "Unlucky Soul"];
-
-    $('#confirm-character-delete').click(function () {
-//        $("#" + $("#char-to-be-deleted")).remove();
-//        index = har_names.indexOf($("#char-to-be-deleted"));
-//        char_names.splice(index,1);
-//        char_levels.splice(index,1);
-//        char_types.splice(index,1);
-        $("#character-select option:selected").remove();
-        $('#deleteModal').modal('hide');
-    });
     
     var inv_items = new Array();
     inv_items.push("Wirt's Leg");
@@ -31,68 +21,36 @@ $(function () {
         }
     });
     
-//    $(document).ready( function () {
-  //      for (var o = 0; o < char_names.length; o++) {
-    //        $("#character-list").append("<li class=\"list-group-item\"> <div class=\"row\"> <div class=\"col-md-3\"> <img src=\"http://placehold.it/40x30\"> </div> <div class=\"col-md-9\"> " + char_names[o] +": Level " + char_levels[o] + " " + char_types[o] + " </div> </div> </li>");
-  //      }
-   // });
-
- /* character tables   var characterRowTemplate = '<tr>' +
-          '<td><input id="char-list" type="checkbox" value="0"></td>' +
-          '<td><a href="character.html#11111"></a></td>' +
-          '<td></td>' +
-          '<td></td>' +
-          '<td></td>' +
-          '<td></td>' +
-        '</tr>';
-
-
-    $.getJSON(
-        "http://lmu-diabolical.appspot.com/characters",
-        function (characters) {
-            // Do something with the character list.
-            characters.forEach(function (character) {
-                var $characterRow = $(characterRowTemplate);
-                $characterRow.find("td:nth-child(2) > a")
-                    .attr({ href: "character.html#" + character.id })
-                    .text(character.name);
-                $characterRow.find("td:nth-child(3)").text(character.classType);
-                $characterRow.find("td:nth-child(4)").text(character.gender.substr(0, 1));
-                $characterRow.find("td:nth-child(5)").text(character.level);
-                $characterRow.find("td:nth-child(6)").text(character.money);
-                $("#characters > tbody").append($characterRow);
-            });
-        }
-    );*/
-    
-    
     $.getJSON(
         "http://lmu-diabolical.appspot.com/characters",
         function(characters) {
             characters.forEach(function(character) {
-                var characterRow = "<option value=" + character.id + ">" + character.name + ": " + "Level " + character.level + " " + character.classType + "</option>";
+                var characterRow = "<option id=\"" + character.id + "\" + value=" + character.name + ">" + character.name + ": " + "Level " + character.level + " " + character.classType + "</option>";
                 $("#character-select").append(characterRow);                
             });
         }
     );
     
     $("#edit-button").click(function() {
-        window.location="character.html/#" + $("#character-select option:selected").val();
+        window.location="character.html/#" + $("#character-select option:selected").attr("id");
     
     });
     
     $("#confirm-character-delete").click(function() { 
+        var idToDelete = $("#character-select option:selected").attr("id");
         $.ajax({
             type: 'DELETE',
-            url: "http://lmu-diabolical.appspot.com/characters/" + $("#character-select option:selected").val(),
+            url: "http://lmu-diabolical.appspot.com/characters/" + idToDelete,
             success: function (data, textStatus, jqXHR) {
-                $("#character-select option:selected").remove();
+                $("#" + idToDelete ).remove();
             }
         });
+        $('#deleteModal').modal('hide');
     });
 
     $("#confirm-character-create").click(function() {
         if ($("#create-char-name").val() && $("#create-char-class").val()) {
+  //      var characterRow = "<option id=\"" + character.id + "\" + value=" + character.name + ">" + character.name + ": " + "Level " + character.level + " " + character.classType + "</option>";
             $.ajax({
                 type: 'POST',
                 url: "http://lmu-diabolical.appspot.com/characters",
@@ -110,6 +68,7 @@ $(function () {
                     // The new character can be accessed from the Location header.
                     console.log("You may access the new character at:" +
                         jqXHR.getResponseHeader("Location"));
+              //      $("#character-select").append(characterRow)
                 }
             });
             $('#createModal').modal('hide');
