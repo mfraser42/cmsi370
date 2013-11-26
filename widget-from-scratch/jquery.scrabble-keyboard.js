8eb8e9c71994e9769275fd1350df0fdd7eb92ae8
Michael Fraser <mfraser702@gmail.com>
2013-11-26 12:14:36 -0800

    widget initial commit

---
+++ widget-from-scratch/jquery.scrabble-keyboard.js
@@ -0,0 +1,27 @@
+(function ($) {
+    
+    $.fn.swivel = function (options) {
+    
+        var keyboard_keys = ['q','w','e','r','t','y','u','i','o','p','|','a','s','d','f','g','h','j','k','l','|','z','x','c','v','b','n','m'],
+            keysLength = keyboard_keys.length,
+            xOffset = 0,
+            yOffset = 0,
+            row = 0;
+        
+        for (var i = 0; i<length; i++)
+        {
+            if (keyboard-keys(i) == '|') {
+                row++;
+                xOffset = row*3 + "%";
+                yOffset += 30 + "%";
+            }
+            $(".keyboard-box").append("<div class=\"key\" id=\"key" + keyboard_keys(i) +">" + keyboard_keys(i) + "</div>");
+            $(".key" + keyboard_keys(i)).css({
+                position: fixed,
+                left: xOffset,
+                top: yOffset    
+            });
+        }
+
+    };
+}(jQuery));
