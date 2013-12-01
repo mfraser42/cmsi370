9c424d623e79fb8e2ba327c6198b620095577356
Michael Fraser <mfraser702@gmail.com>
2013-11-30 23:01:41 -0800

    Keyboard and input box spacing/layout introduced

--- widget-from-scratch/jquery.scrabble-keyboard.js
+++ widget-from-scratch/jquery.scrabble-keyboard.js
@@ -5,17 +5,22 @@
             $current = null;
     
       //  $this.addClass("keyboard-box")
+        var $key = $('<input type="text" class="drop-area"> </input>');
+        $this.append($key);
+        
+        var $key = $('<div class="keyboard-box"> </div>"');
+        $this.append($key);
 
-        var keyboard_keys = ['q','w','e','r','t','y','u','i','o','p','|','a','s','d','f','g','h','j','k','l','|','z','x','c','v','b','n','m'],
+        var keyboard_keys = ['q','w','e','r','t','y','u','i','o','p','|','a','s','d','f','g','h','j','k','l','|','z','x','c','v','b','n','m','sp'],
             keysLength = keyboard_keys.length,
             xOffset = 0,
-            yOffset = 0,
+            yOffset = 30,
             row = 0;
 
         keyboard_keys.forEach(function (character) {
             if (character === '|') {
                 row++;
-                yOffset += 20;
+                yOffset += 23;
                 xOffset = 3*row;
             } else {
                 var $key = $('<div class="key ' + character + '">' + character + "</div>");
@@ -25,9 +30,11 @@
                     left: xOffset + '%',
                     top: yOffset + '%'    
                 });
-                xOffset += 5;
+                xOffset += 6;
             }
         });
+        
+        
 
     };
 }(jQuery));
