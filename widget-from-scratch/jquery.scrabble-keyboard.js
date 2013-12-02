80a3a058bfb6063829c4a1d58426903194a68262
Michael Fraser <mfraser702@gmail.com>
2013-12-01 20:31:20 -0800

    removed unnecessary code from the boxes variable that allowed for the creation of boxes

--- widget-from-scratch/jquery.scrabble-keyboard.js
+++ widget-from-scratch/jquery.scrabble-keyboard.js
@@ -34,7 +34,6 @@
                 jQueryElements
                     .addClass("drawing-area")
                     // "this" is Boxes.
-                    .mousedown(this.startDraw)
                     .mousemove(this.trackDrag)
                     
                     // We conclude drawing on either a mouseup or a mouseleave.
@@ -53,45 +52,11 @@
             },
 
             /**
-             * Begins a box draw sequence.
-             */
-            startDraw: function (event) {
-                // We only respond to the left mouse button.
-                if (event.which === Boxes.LEFT_BUTTON) {
-                    // Add a new box to the drawing area.  Note how we use
-                    // the drawing area as a holder of "local" variables
-                    // ("this" as standardized by jQuery).
-                    this.anchorX = event.pageX;
-                    this.anchorY = event.pageY;
-                    this.drawingBox = $("<div></div>")
-                        .appendTo(this)
-                        .addClass("box")
-                        .offset({ left: this.anchorX, top: this.anchorY });
-
-                    // Take away the highlight behavior while the draw is
-                    // happening.
-                    Boxes.setupDragState();
-                }
-            },
-
-            /**
              * Tracks a box as it is rubberbanded or moved across the drawing area.
              */
             trackDrag: function (event) {
-                // Don't bother if we aren't tracking anything.
-                if (this.drawingBox) {
-                    // Calculate the new box location and dimensions.  Note how
-                    // this might require a "corner switch."
-                    var newOffset = {
-                        left: (this.anchorX < event.pageX) ? this.anchorX : event.pageX,
-                        top: (this.anchorY < event.pageY) ? this.anchorY : event.pageY
-                    };
-
-                    this.drawingBox
-                        .offset(newOffset)
-                        .width(Math.abs(event.pageX - this.anchorX))
-                        .height(Math.abs(event.pageY - this.anchorY));
-                } else if (this.movingBox) {
+                // Don't bother if we aren't moving anything.
+                if (this.movingBox) {
                     // Reposition the object.
                     this.movingBox.offset({
                         left: event.pageX - this.deltaX,
@@ -104,16 +69,7 @@
              * Concludes a drawing or moving sequence.
              */
             endDrag: function (event) {
-                if (this.drawingBox) {
-                    // Finalize things by setting the box's behavior.
-                    this.drawingBox
-                        .mousemove(Boxes.highlight)
-                        .mouseleave(Boxes.unhighlight)
-                        .mousedown(Boxes.startMove);
-                    
-                    // All done.
-                    this.drawingBox = null;
-                } else if (this.movingBox) {
+                if (this.movingBox) {
                     // Change state to "not-moving-anything" by clearing out
                     // this.movingBox.
                     this.movingBox = null;
